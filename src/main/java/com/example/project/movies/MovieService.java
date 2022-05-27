96a49dbd6403026bb1835dfc6504a07b3d4bcace
Georgii Semenov <george.sem@live.ru>
2022-05-27 19:21:35 +0300

    обсудим лично :)

--- src/main/java/com/example/project/movies/MovieService.java
+++ src/main/java/com/example/project/movies/MovieService.java
@@ -1,25 +1,66 @@
 package com.example.project.movies;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 @Service
 public class MovieService {
 
-    public ArrayList<ChatDialog> getMovies() {
-        ChatDialog A = new ChatDialog();
-        A.name = "Hermione";
-        A.message = "It's leviOsa, not leviosA!";
+    public ArrayList<Movie> getMovies() {
+    // TODO:
 
-        ChatDialog B = new ChatDialog();
-        B.name = "Margarita";
-        B.message = "It is vodka, isn't it?";
 
-        ArrayList<ChatDialog> movies = new ArrayList<ChatDialog>();
-        movies.add(A);
-        movies.add(B);
-        return movies;
+    }
+
+
+    public String[] findMovies(String s) {
+
+        try {
+
+            URL url = new URL("https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?" +
+                    "keyword=" + URLEncoder.encode(s, StandardCharsets.UTF_8) +
+                    "&page=1");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("X-API-KEY", "79309bde-6cab-48d6-a717-e633cf5fbc89");
+
+            int responsecode = conn.getResponseCode();
+
+            if (responsecode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responsecode);
+            } else {
+
+                String inline = "";
+                Scanner scanner = new Scanner(conn.getInputStream());
+                while (scanner.hasNext()) {
+                    inline += scanner.nextLine();
+                }
+                scanner.close();
+                JSONObject response = new JSONObject(inline);
+
+                // Здесь в бой вступаем МЫ!
+                JSONArray movies = (JSONArray) response.get("films");
+                for (int i = 0; i < movies.length(); i++) {
+                    System.out.println(movies.getJSONObject(i).getString("nameRu"));
+                }
+
+            }
+
+            conn.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new String[]{};
 
     }
 
